3e8d6a227a033ec149b89166c5529d571aa30724
Antti Vepsäläinen <apvepsala@gmail.com>
2023-05-28 19:50:28 +0300

    Change project structure to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,2 @@
 from setuptools import setup
-
-setup(name='pyqsl',
-      version='1.1',
-      description='A simple simulation framework designed to solve problems in quantum physics.',
-      url='https://github.com/ittnas/pyqsl/',
-      author='Antti Vepsäläinen',
-      author_email='avepsala@mit.edu',
-      license='MIT',
-      packages=['pyqsl'],
-      install_requires=['packaging'],
-      zip_safe=True)
+setup()
