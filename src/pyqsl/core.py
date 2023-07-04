Fixes in the simulation result broadcasting.
Also add a new example.

src/pyqsl/core.py
@@ -54,14 +54,21 @@ def _simulation_loop_body(
     settings_with_relations = settings.resolve_relations()
     settings_dict = settings.to_dict()
     invalid_args = _get_invalid_args(task, settings_dict)
-    logger.debug('Removing invalid args ({str(invalid_args)}) from function.')
-    valid_settings = {key: settings_dict[key] for key in settings_dict if key not in invalid_args}
+    logger.debug("Removing invalid args ({str(invalid_args)}) from function.")
+    valid_settings = {
+        key: settings_dict[key] for key in settings_dict if key not in invalid_args
+    }
     output = task(**valid_settings)
 
     if post_processing_in_the_loop:
         output = post_processing_in_the_loop(output, **settings_dict)
 
-    output_as_dict = {'output': output, 'settings_with_relations': {key: settings_dict[key] for key in settings_with_relations}}
+    output_as_dict = {
+        "output": output,
+        "settings_with_relations": {
+            key: settings_dict[key] for key in settings_with_relations
+        },
+    }
     return output_as_dict
 
 
@@ -164,16 +171,27 @@ def run(
         + str(end_time - start_time)
         + "."
     )
-    dataset = _create_dataset(output_array, settings, sweeps, expand_data=expand_data, dims=dims)
+    dataset = _create_dataset(
+        output_array, settings, sweeps, expand_data=expand_data, dims=dims
+    )
     simulation_result = SimulationResult(dataset)
     return simulation_result
 
 
-def _create_dataset(output_array: Any, settings: Settings, sweeps: dict[Union[str, Setting], Any], expand_data, dims)->xr.Dataset:
+def _create_dataset(
+    output_array: Any,
+    settings: Settings,
+    sweeps: dict[Union[str, Setting], Any],
+    expand_data,
+    dims,
+) -> xr.Dataset:
     """
     Creates xarray dataset from simulation results.
     """
-    coords = {setting.name if isinstance(setting, Setting) else setting: data for setting, data in sweeps.items()}
+    coords = {
+        setting.name if isinstance(setting, Setting) else setting: data
+        for setting, data in sweeps.items()
+    }
     dataset: xr.Dataset
 
     data_vars: dict[str, Any] = {}
@@ -191,17 +209,17 @@ def _create_dataset(output_array: Any, settings: Settings, sweeps: dict[Union[st
         else:
             new_dims = dims.copy()
         new_dims.extend(new_shape)
-        temporary_array[key] = np.reshape(
-            np.array(temporary_array[key]), new_dims
-        )
+        temporary_array[key] = np.reshape(np.array(temporary_array[key]), new_dims)
 
     expanded_output = temporary_array
-    output_array = expanded_output['output']
-    relation_array = expanded_output['settings_with_relations']
+    output_array = expanded_output["output"]
+    relation_array = expanded_output["settings_with_relations"]
     relation_dict: dict[str, Any]
     if len(dims) == 0:
         output_array_reshaped = output_array[()]
-        relation_dict = {f'{key}_evaluated': value for key, value in relation_array[()].items()}
+        relation_dict = {
+            f"{key}_evaluated": value for key, value in relation_array[()].items()
+        }
     else:
         try:
             output_array_reshaped = np.reshape(np.array(output_array), dims)
@@ -209,17 +227,17 @@ def _create_dataset(output_array: Any, settings: Settings, sweeps: dict[Union[st
             output_array_reshaped = np.reshape(np.array(output_array), dims + [-1])
         relation_dict = {}
         for key in relation_array.flat[0]:
-            relation_dict[f'{key}_evaluated'] = []
+            relation_dict[f"{key}_evaluated"] = []
         for ii in range(relation_array.size):
-            for key_rd, key_ra in zip(relation_dict.keys(), relation_array.flat[0].keys()):
+            for key_rd, key_ra in zip(
+                relation_dict.keys(), relation_array.flat[0].keys()
+            ):
                 relation_dict[key_rd].append(relation_array.flat[ii][key_ra])
         for key in relation_dict:
             new_shape = np.array(relation_dict[key]).shape[1:]
             new_dims = dims.copy()
             new_dims.extend(new_shape)
-            relation_dict[key] = np.reshape(
-                np.array(relation_dict[key]), new_dims
-            )
+            relation_dict[key] = np.reshape(np.array(relation_dict[key]), new_dims)
 
     if expand_data:
         if isinstance(output_array.flat[0], dict):
@@ -241,28 +259,36 @@ def _create_dataset(output_array: Any, settings: Settings, sweeps: dict[Union[st
                 )
             for key in temporary_array:
                 data_vars[key] = (tuple(coords), temporary_array[key])
-            #return temporary_array
+            # return temporary_array
         elif output_array.shape != tuple(dims):
             # Iterable that has been converted to np.array
             for ii in range(len(dims)):
                 output_array_reshaped = np.moveaxis(output_array_reshaped, 0, -1)
-            extended_coords = tuple([f'index_{ii}' for ii in range(len(output_array_reshaped.shape) - len(dims))]) + tuple(coords)
-            data_vars['data'] = (extended_coords, output_array_reshaped)
+            extended_coords = tuple(
+                [
+                    f"index_{ii}"
+                    for ii in range(len(output_array_reshaped.shape) - len(dims))
+                ]
+            ) + tuple(coords)
+            data_vars["data"] = (extended_coords, output_array_reshaped)
         else:
             # Fallback to default behaviour without expanding
-            data_vars['data'] = (tuple(coords), output_array_reshaped)
+            data_vars["data"] = (tuple(coords), output_array_reshaped)
     else:
-        extended_coords = tuple(coords) + tuple([f'index_{ii}' for ii in range(len(output_array_reshaped.shape) - len(dims))])
-        data_vars['data'] = (tuple(extended_coords), output_array_reshaped)
+        extended_coords = tuple(coords) + tuple(
+            [
+                f"index_{ii}"
+                for ii in range(len(output_array_reshaped.shape) - len(dims))
+            ]
+        )
+        data_vars["data"] = (tuple(extended_coords), output_array_reshaped)
 
     for setting_name, value in relation_dict.items():
         data_vars[setting_name] = (tuple(coords), value)
 
-    #print(data_vars, coords)
+    # print(data_vars, coords)
     dataset = xr.Dataset(
-        data_vars=data_vars,
-        coords=coords,
-        attrs={'settings': settings}
+        data_vars=data_vars, coords=coords, attrs={"settings": settings}
     )
 
     return dataset
@@ -273,7 +299,7 @@ def _get_invalid_args(func, argdict):
     Get set of invalid arguments for a function.
     https://stackoverflow.com/questions/196960/can-you-list-the-keyword-arguments-a-function-receives
     """
-    args, varargs, varkw, _, kwonlyargs, _,  _ = inspect.getfullargspec(func)
+    args, varargs, varkw, _, kwonlyargs, _, _ = inspect.getfullargspec(func)
     if varkw:
         return set()  # All accepted
     return set(argdict) - set(args)

src/pyqsl/relation.py
@@ -19,13 +19,15 @@ import numexpr as ne
 import numpy as np
 import logging
 from scipy.interpolate import interpn
+
 logger = logging.getLogger(__name__)
+
 
 @dataclasses.dataclass
 class Relation(ABC):
     """
     Relation represents a mathematical relation between different setting values in the settings hierarchy.
-    
+
     The relations are represented through the attribute paremeters, which maps the parameters used in the
     relation to other setting names, or alternatively, to other relations. How the related paremeters are used
     is controlled by the evaluate-method, which is called during relation resolution. The evaluate method can be
@@ -41,8 +43,11 @@ class Relation(ABC):
             The resulting value of the relation evaluation. None implies that the relation has not been
             resolved.
     """
+
     parameters: dict[str, Union[str, "Relation"]]
-    _parameters: dict[str, Union[str, "Relation"]] = dataclasses.field(init=False, repr=False)
+    _parameters: dict[str, Union[str, "Relation"]] = dataclasses.field(
+        init=False, repr=False
+    )
     evaluated_value: Optional[Any] = None
 
     @property
@@ -77,7 +82,9 @@ class Relation(ABC):
         mapped_setting_names = set()
         for setting_or_relation in self.parameters.values():
             if isinstance(setting_or_relation, Relation):
-                mapped_setting_names.update(setting_or_relation.get_mapped_setting_names())
+                mapped_setting_names.update(
+                    setting_or_relation.get_mapped_setting_names()
+                )
             else:
                 mapped_setting_names.add(setting_or_relation)
         return mapped_setting_names
@@ -123,7 +130,8 @@ class Equation(Relation):
             The implemented equation in string format. The equation represented by the string
             has to be interpretable by numexpr. By default the equation just returns 0.
     """
-    equation: str = '0'
+
+    equation: str = "0"
 
     def __post_init__(self):
         """
@@ -190,9 +198,13 @@ class LookupTable(Relation):
 
         data_array = np.array(self.data, dtype=object)
         data_shape = data_array.shape
-        coordinate_shape = tuple([len(coordinate_values) for coordinate_values in self.coordinates.values()])
+        coordinate_shape = tuple(
+            [len(coordinate_values) for coordinate_values in self.coordinates.values()]
+        )
         if data_shape != coordinate_shape:
-            raise ValueError(f"Data shape is different from coordinate dimensions, {data_shape} != {coordinate_shape}")
+            raise ValueError(
+                f"Data shape is different from coordinate dimensions, {data_shape} != {coordinate_shape}"
+            )
 
     def evaluate(self, **parameter_values):
         """
@@ -212,6 +224,5 @@ class LookupTable(Relation):
         return output
 
     def __str__(self):
-        output = "LookupTable for " + ', '.join(self.coordinates)
+        output = "LookupTable for " + ", ".join(self.coordinates)
         return output
-

src/pyqsl/simulation_result.py
@@ -8,12 +8,27 @@ from typing import Any, Optional
 import xarray as xr
 from pyqsl.settings import Settings
 import numpy as np
+import copy
+import dataclasses
+import json
+import pickle
 
 
-class SimulationResult():
+class SimulationResult:
     """
     This class stores the simulation results and the settings used to generate it.
+
+    Simulation result is a wrapper around xarray, which is used as the main data structure.
+    The underlying xarray dataset can be accessed through ```dataset``` attribute.
+
+    The settings, results and sweeps can be accessed as attributes of the simulation result.
+
+    There are two ways of accesssing settings, one can either directly call e.g. ```simulation_result.amplitude```
+    in which case the only setting values are returned and broadcasted to sweep dimensions.
+
+    Another option is to access ```simulation_result.settings.amplitude```, which returns the setting object.
     """
+
     dataset: xr.Dataset
 
     def __init__(self, dataset: Optional[xr.Dataset] = None):
@@ -26,20 +41,37 @@ class SimulationResult():
         """
         Getting attribute of the simulation result fetches a data_var from the underlying Dataset.
         """
-        if key == 'settings':
-            return self.dataset.attrs['settings']
+        if key == "settings":
+            return self.dataset.attrs["settings"]
+        if key == "sweeps":
+            return {coord: value.values for coord, value in self.dataset.coords.items()}
         if key in self.dataset.data_vars:
             da = self.dataset[key]
             if len(da.dims) == 0:
                 return da.values[()]
             return da.values
-        if 'settings' in self.dataset.attrs and key in self.dataset.attrs['settings']:
-            if f'{key}_evaluated' in self.dataset.data_vars:
-                return self.dataset[f'{key}_evaluated'].values if len(self.dataset[f'{key}_evaluated'].dims) > 0 else self.dataset[f'{key}_evaluated'].values[()]
+        if "settings" in self.dataset.attrs and key in self.dataset.attrs["settings"]:
+            if f"{key}_evaluated" in self.dataset.data_vars:
+                return (
+                    self.dataset[f"{key}_evaluated"].values
+                    if len(self.dataset[f"{key}_evaluated"].dims) > 0
+                    else self.dataset[f"{key}_evaluated"].values[()]
+                )
             model_data_array = self.dataset[list(self.dataset.data_vars)[0]]
             if key in self.dataset.coords:
-                return np.broadcast_to(np.array(self.dataset.coords[key].values), model_data_array.shape, subok=True)
-            return np.broadcast_to(self.dataset.attrs['settings'][key].value, model_data_array.shape, subok=True)
+                broadcast_shape = [1]*len(model_data_array.shape)
+                broadcast_shape[model_data_array.dims.index(key)] = -1
+                reshaped = np.reshape(np.array(self.dataset.coords[key].values), broadcast_shape)
+                return np.broadcast_to(
+                    reshaped,
+                    model_data_array.shape,
+                    subok=True,
+                )
+            return np.broadcast_to(
+                self.dataset.attrs["settings"][key].value,
+                model_data_array.shape,
+                subok=True,
+            )
         raise AttributeError()
 
     def __dir__(self):
@@ -47,9 +79,32 @@ class SimulationResult():
         Add attributes created by __getattr__.
         """
         items = list(self.__dict__.keys()) + list(self.dataset.data_vars)
-        if 'settings' in self.dataset.attrs:
-            items = items + ['settings']
+        if "settings" in self.dataset.attrs:
+            items = items + ["settings"] + ["sweeps"]
         return items
 
     def __str__(self):
         return str(self.dataset)
+
+    def save(self, path: str):
+        """
+        Saves the simulation result to a pickle file.
+
+        Args:
+            path: savepath
+        """
+        with open(path, "wb") as f:
+            pickle.dump(self.dataset, f)
+
+
+def load(path: str) -> SimulationResult:
+    """
+    Loads the dataset from pickle file.
+
+    Args:
+        path: Load path
+    """
+    with open(path, "rb") as f:
+        dataset = pickle.load(f)
+    simulation_result = SimulationResult(dataset)
+    return simulation_result